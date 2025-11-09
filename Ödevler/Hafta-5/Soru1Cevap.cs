658e7f8ef9a7fd9bdcb783629554a61ba5df7113
ummugulsum08040 <ummgulsmylmzz12@gmail.com>
2025-11-09 13:52:43 +0300

    Update Soru1Cevap.cs

--- Ödevler/Hafta-5/Soru1Cevap.cs
+++ Ödevler/Hafta-5/Soru1Cevap.cs
@@ -4,8 +4,38 @@ namespace Cevap
     {
         static void Main(string[] args)
         {
-            // Çözümlerinizi buraya yazın.
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+ int[,]matris=new int[5,3];
+ for (int i = 0; i < matris.GetLength(0); i++)
+ {
+
+     for (int j = 0; j < matris.GetLength(1); j++)
+     {
+         matris[i, j] = rnd.Next(0, 9);
+         Console.Write(matris[i,j]+" ");
+     }
+
+     Console.WriteLine();
+ }
+ int[] sütuntoplam = new int[3];
+ for(int i = 0;i < 3; i++)
+ {
+     //her sütun için o sütundaki satıları topluyoruz ondan satır döngüsü içte
+     for(int j = 0; j < 5; j++)
+     {
+         sütuntoplam[i] += matris[j,i];
+     }
+   
+ }
+          
+ for(int i = 0; i < sütuntoplam.Length; i++)
+ {
+     Console.WriteLine("Sütun" + (i+1) + ":" + sütuntoplam[i]);
+ }
+
+               
+
+            
         }
     }
 }
